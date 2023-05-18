07c3bf661093b1a4c060230df458f7988dbead47
Adam Hrubý <adam.hruby@newlogic.cz>
2023-05-18 14:37:16 +0200

    second day second commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function triggermobile() {
+    var div = document.getElementById("show");
+    if (div.style.display === "none") {
+        div.style.display = "flex";
+    } else {
+        div.style.display = "none";
+    }
+}
